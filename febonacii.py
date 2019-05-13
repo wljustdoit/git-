cbfeed5f20c739166adc9f3a3ba45a979661168e
shen <565373853@qq.com>
2019-05-13 21:24:09 +0800

    斐波那契数列

---
+++ febonacii.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding=utf-8
+def febonacii(num):
+    a = 0
+    b = 1
+    current_index = 0
+    while current_index < num :
+        yield a
+        a, b = b , a + b
+        current_index += 1
+
+
+def main():
+    try:
+        num1 = int(input("请输入数字"))
+        result = febonacii(num1)
+        for value in result:
+            print(value)
+    except EXCEPTION as e:
+        print(e)
+    finally:
+        print("byebye")
+        return
+
+
+if __name__ == "__main__":
+        main()
